8f5da956412dbb3882212caec42222deea939830
Maarten Louage <me@maartenlouage.nl>
2023-04-19 15:17:57 +0200

    Hello world GraphQL server

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { gql } from "graphql-tag";
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello!",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const startServer = async () => {
+  const { url } = await startStandaloneServer(server, {
+    listen: {
+      port: 4000,
+    },
+  });
+  console.log(`🚀 Server ready at ${url}`);
+};
+
+startServer();
